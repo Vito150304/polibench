6a53580e56d534a77026b35bc41d2a05ea6ca50b
Vito Burdi <v.burdi4@studenti.poliba.it>
2026-08-16 18:21:49 +0200

    Verification email, resent email feature

---
+++ frontend/src/pages/EmailSentPage.jsx
@@ -0,0 +1,9 @@
+function VerificationEmailPage() {
+    return (
+        <div>
+            <h1>Registration Successful: Verification Email Sent</h1>
+            <p>Please check your email to verify your account</p>
+        </div>
+    );
+}
+export default VerificationEmailPage;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import  useAuth  from '../contexts/AuthContext';
 import { useRef } from 'react';
+import { useMutation } from '@tanstack/react-query';
 
 function LoginPage() {
 
@@ -14,13 +15,61 @@ function LoginPage() {
         
         login(email, password);
     }
-    
-    return(
+
+    const NewVerificationEmailMutation = useMutation({
+        mutationKey: ['NewVerificationEmail'],
+        mutationFn: async ({email}) => {
+            let response;
+            try {
+                response = await fetch('/api/v1/users/resend-verification', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ email })
+                });
+            } catch (networkError) {
+                console.error("Errore durante la fetch:", networkError);
+                throw new Error('Network error during resend verification email', { cause: networkError });
+            }
+
+            if (!response.ok) {
+                const errorData = await response.json().catch(() => ({}));
+                throw new Error(errorData.detail || 'Failed to resend verification email');
+            }
+
+            return await response.json();
+        },
+
+        onSuccess: () => {
+            alert('Email has been sent again');
+        },
+
+        onError: (error) => {
+            console.error("Errore durante l'invio della email di verifica:", error);
+        }
+    });
+
+    const handleNewVerificationEmail = () => {
+        const currentEmail = emailRef.current.value;
+        
+        if (!currentEmail) {
+            alert("Per favore, inserisci la tua email nel form prima di richiedere un nuovo link.");
+            return; 
+        }
+        
+        NewVerificationEmailMutation.mutate({ email: currentEmail });
+    }
+
+    return( //Ci devo mettere l'interfaccia per login con Google
+        <>
         <form onSubmit = {handleSubmit}>
             <input type = "email" ref = {emailRef} placeholder = "Email" required />
             <input type = "password" ref = {passwordRef} placeholder = "Password" required />
             <button type = "submit">Login</button>
         </form>
+        <button onClick = {handleNewVerificationEmail}>Resend email.</button>
+        </>
     )
 }
 export default LoginPage;
